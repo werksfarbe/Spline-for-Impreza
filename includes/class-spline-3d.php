6ef429099ec75fc46743ed3f279165a745131127
Tom <tom@iblacky-2000.local>
2023-08-23 16:56:26 +0200

    final

--- includes/class-spline-3d.php
+++ includes/class-spline-3d.php
@@ -3,39 +3,69 @@
 add_action( 'vc_after_init', 'add_custom_3d_object_field' );
 
 function add_custom_3d_object_field() {
+	// Custom object
 	$attributes_textfield = array(
 		'type' => 'textfield',
-		'heading' => '3D-Objekt',
+		'heading' => '3D-Object',
 		'param_name' => 'custom_3d_object',
-		'description' => 'Geben Sie hier den Pfad zu Ihrem 3D-Objekt ein. NUR den Pfad aus dem spline-viewer. Keinen Code.',
+		'description' => 'Pash to spline viewer file.',
 	);
 	vc_add_param( 'vc_row', $attributes_textfield );
-	// Custom checkbox
+	// Custom Option Sticky
 	$attributes_checkbox = array(
 		'type' => 'checkbox',
-		'heading' => 'Objekt ist sticky',
+		'heading' => 'Object is sticky',
 		'param_name' => 'custom_3d_object_sticky',
 		'value' => array('Ja' => 'true'),
 		'description' => 'Aktivieren Sie diese Option, um das 3D-Objekt als sticky zu markieren.',
 	);
 	vc_add_param( 'vc_row', $attributes_checkbox );
+	// Custom Height
+	$attributes_height = array(
+		'type' => 'textfield',
+		'heading' => 'Object Height',
+		'param_name' => 'custom_3d_height',
+		'description' => 'Set height of the object.',
+		'value' => '100vh' // This sets the default value to 100vh
+	);
+	vc_add_param( 'vc_row', $attributes_height );
+	// Custom ID
+	$attributes_id = array(
+		'type' => 'textfield',
+		'heading' => 'Object ID',
+		'param_name' => 'custom_3d_id',
+		'description' => 'Set ID if you want to manipulate the view via css.',
+	);
+	vc_add_param( 'vc_row', $attributes_id );
 }
 
 add_filter( 'vc_shortcode_output', 'prepend_custom_3d_object_inside_row', 10, 3 );
 
 
-/* Add spline Viewer in Row */
+/* Add spline Viewer in Frontend Row */
 
 function prepend_custom_3d_object_inside_row( $output, $shortcode, $atts ) {
 	if ( $shortcode->settings('base') === 'vc_row' && isset( $atts['custom_3d_object'] ) && ! empty( $atts['custom_3d_object'] ) ) {
-		$custom_output = '<div class="custom-3d-object';
-
+		
 		// If the checkbox is true, add the class "object_is_sticky" to the div
+		$classes = 'custom-3d-object';
 		if (isset($atts['custom_3d_object_sticky']) && $atts['custom_3d_object_sticky'] === 'true') {
-			$custom_output .= ' object_is_sticky';
+			$classes .= ' object_is_sticky';
 		}
 
-		$custom_output .= '">';
+		// Add the ID attribute if custom_3d_id is set
+		$id_attribute = '';
+		if (isset($atts['custom_3d_id']) && !empty($atts['custom_3d_id'])) {
+			$id_attribute = ' id="' . esc_attr($atts['custom_3d_id']) . '"';
+		}
+
+		// Add height style if custom_3d_height is set
+		$height_style = '';
+		if (isset($atts['custom_3d_height']) && !empty($atts['custom_3d_height'])) {
+			$height_style = ' style="height: ' . esc_attr($atts['custom_3d_height']) . ';"'; // Assuming the value includes units like 'px' or '%'
+		}
+
+		$custom_output = '<div class="' . $classes . '"' . $id_attribute . $height_style . '>';
 		$custom_output .= '<spline-viewer loading-anim url="' . esc_url( $atts['custom_3d_object'] ) . '"></spline-viewer>';
 		$custom_output .= '</div>';
 
@@ -44,23 +74,3 @@ function prepend_custom_3d_object_inside_row( $output, $shortcode, $atts ) {
 	}
 	return $output;
 }
-/* has_3d_object Class */
-function check_for_3d_object($content) {
-	// Überprüfen Sie, ob der spezifische URL-Anfang im Inhalt vorhanden ist
-	if (strpos($content, 'https://prod.spline.design') !== false) {
-		// Setzen Sie eine globale Variable, um später zu überprüfen
-		global $has_3d_object;
-		$has_3d_object = true;
-	}
-	return $content;
-}
-add_filter('the_content', 'check_for_3d_object', 20);  // Priority 20, um sicherzustellen, dass es nach anderen Filtern ausgeführt wird
-
-function add_3d_object_class($classes) {
-	global $has_3d_object;
-	if ($has_3d_object) {
-		$classes[] = 'has_3d_object';
-	}
-	return $classes;
-}
-add_filter('body_class', 'add_3d_object_class');
