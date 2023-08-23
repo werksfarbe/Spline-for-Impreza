9e4bf741a811937956819b21671a67aa78295314
Tom <tom@iblacky-2000.local>
2023-08-23 17:46:41 +0200

    backend tool

--- includes/class-spline-admin.php
+++ includes/class-spline-admin.php
@@ -15,18 +15,20 @@ function spline_3d_admin_subpage() {
 
 	$script_url = get_option('spline_3d_script_url', 'https://unpkg.com/@splinetool/viewer/build/spline-viewer.js');
 	?>
-	<div class="wrap">
+	<div class="wrap" style="max-width: 600px;">
 		<h2>Spline 3D</h2>
-		<img src="<?php echo plugin_dir_url(__FILE__) . 'images/backend-header.png'; ?>" alt="Backend Header" style="width: 100%; max-width: 600px; display: block; margin-bottom: 20px;">
-		<form method="post" action="">
-			<table class="form-table">
-				<tr valign="top">
-				<th scope="row">JavaScript URL</th>
-				<td><input type="text" name="spline_3d_script_url" value="<?php echo esc_url($script_url); ?>" /></td>
-				</tr>
-			</table>
-			<?php submit_button(); ?>
-		</form>
+		<img src="<?php echo SPLINE_3D_URL . 'images/backend-header.png'; ?>" alt="Backend Header" style="width: 100%; max-width: 600px; display: block;">
+		<div class="form-wrapper" style=" background: #ffffff; padding: 1rem;">
+			<form method="post" action="">
+				<table class="form-table">
+					<tr valign="top">
+					<th scope="row">spline-viewer.js URL</th>
+					<td><input type="text" name="spline_3d_script_url" value="<?php echo esc_url($script_url); ?>" style="width:100%" /></td>
+					</tr>
+				</table>
+				<?php submit_button(); ?>
+			</form>
+		</div>
 	</div>
 	<?php
 }
